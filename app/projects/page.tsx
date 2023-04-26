11198cdb99e5965b70127b59e884ec0baa758f5f
Adam Hughes <adamjordanhughes@gmail.com>
2023-04-26 18:06:27 -0400

    Fix ordering of projects on smaller screens

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -11,38 +11,17 @@ export default function ProjectsPage() {
 		<div className="relative pb-16">
 			<Navigation />
 			<div className="px-6 pt-16 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
-				<div className="hidden w-full h-px md:block bg-zinc-800" /> 
-
-				<div className="grid  grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
-					<div className="grid grid-cols-1 gap-4">
-						{sorted
-							.filter((_, i) => i % 3 === 0)
-							.map((project) => (
-								<Card key={project.id}>
-									<Article project={project}  />
-								</Card>
-							))}
-					</div>
-					<div className="grid grid-cols-1 gap-4">
-						{sorted
-							.filter((_, i) => i % 3 === 1)
-							.map((project) => (
-								<Card key={project.id}>
-									<Article project={project} />
-								</Card>
-							))}
-					</div>
-					<div className="grid grid-cols-1 gap-4">
-						{sorted
-							.filter((_, i) => i % 3 === 2)
-							.map((project) => (
-								<Card key={project.id}>
-									<Article project={project} />
-								</Card>
-							))}
-					</div>
+				<div className="hidden w-full h-px md:block bg-zinc-800" />
+				<div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
+					{sorted.map((project) => (
+						<div className="grid grid-cols-1 gap-4" key={project.id}>
+							<Card>
+								<Article project={project} />
+							</Card>
+						</div>
+					))}
 				</div>
 			</div>
-		</div> 
+		</div>
 	);
 }
